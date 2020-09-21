975869b0a32c39f089b6a056c74e52adda8a8653
dreamli <dreamli2018@outlook.com>
2020-09-21 15:44:41 +0800

    修改启动类注解

--- center-user/center-user-service/src/main/java/com/cranberries/user/UserApplication.java
+++ center-user/center-user-service/src/main/java/com/cranberries/user/UserApplication.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com/cranberries/user/mapper")
 @EnableDiscoveryClient
 @EnableCaching
 @EnableScheduling
+@MapperScan(basePackages = "com/cranberries/user/mapper")
 public class UserApplication {
 
     public static void main(String[] args) {
